e02bf8f0996995560ab714056bfbb152ce42f55d
Kiwi Steve <Kiwi Steve@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-16 16:42:09 +0000

    Initial release.
    git-svn-id: https://plugins.svn.wordpress.org/kit-days-away/trunk@573109 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ kit-days-away.php
@@ -0,0 +1,54 @@
+<?php
+/*
+Plugin Name: Kiwi IT - Days Away
+Plugin URI: http://kiwi-it.co.uk/?page_id=435
+Description: Displays the number of days until/after a specified date.  Example: [kit-days-away day=25 month=12 year=2012]
+Version: 1.0
+Author: Steve Mosen
+Author URI: http://kiwi-it.co.uk
+License: GPL2
+
+Copyright 2012  Steve Mosen  (email : Steve.Mosen@Kiwi-IT.co.uk)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as 
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+function kit_days_away( $atts ) {
+	extract( shortcode_atts( array(
+		'day' 		=> 1,
+		'month'		=> 1,
+		'year'		=> 1960,
+		'offset'	=> 0,
+		'before'	=> "Event occurs",
+		'after'		=> "Event occurred",
+		'today'		=> ""
+	), $atts ) );
+
+	$days = (int)((time(void) + ($offset * 3600) - mktime(0, 0, 0, $month, $day, $year)) / 86400);
+
+	if ( $days < 0  && $before != "" ) {
+		$result = $before . " in " . absint($days) . " days";
+	}
+	elseif ( $days > 0 && $after != "" ) {
+		$result = $after . " " . $days . " days ago";
+	}
+	elseif ( 0 == $days ) {
+		$result = $today;
+	}
+
+	return $result;
+}
+add_shortcode( 'kit-days-away', 'kit_days_away' );
+
+?>
---
+++ readme.txt
@@ -0,0 +1,35 @@
+=== Plugin Name ===
+Contributors: Kiwi Steve
+Tags: countdown, timer
+Requires at least: 3.4.1
+Tested up to: 3.4.1
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Displays the number of days until/after a specified date.
+
+== Description ==
+
+Displays the number of days until/after a specified date.  Example: [kit-days-away day=25 month=12 year=2012]
+
+== Installation ==
+
+1. Upload the `kiwi-it-days-away` folder to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Add the appropriate shortcode to your pages and posts.
+
+== Frequently Asked Questions ==
+
+= Why another day counter? =
+
+I couldn't find a countdown timer that did exactly what I wanted (plain text, custom messages), so I created this.
+
+== Screenshots ==
+
+None.
+
+== Changelog ==
+
+= 1.0 =
+* Initial release.
